d6e51f4bde1e3152d1136237e9df27b125d4249c
Isaiah Jenkins <isaiahjenkins@isaiahs-mbp>
2016-07-29 23:03:19 -0700

    Initial Commit

---
+++ MasqueParty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasqueParty
+//
+//  Created by Isaiah Jenkins on 7/29/16.
+//  Copyright © 2016 MasqueParty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
