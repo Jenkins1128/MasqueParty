a8a8382c4e14fd6ff03b7e663e3bb6468dfb8e31
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2021-10-26 21:52:12 -0700

    Adding other async tests

--- MasquePartyTests/MasquePartyFirebaseTests.swift
+++ MasquePartyTests/MasquePartyFirebaseTests.swift
@@ -44,22 +44,96 @@ class MasquePartyFirebaseTests: XCTestCase {
                 XCTFail("Error: \(error?.localizedDescription ?? "")")
                 return
             }
-            print("document data", document?.data() ?? "No document")
             if let document = document, document.exists {
-                
                 if document.get(field) != nil {
                     exists = true
-                    print("field exists")
-                }else{
-                    print("field nil")
                 }
             }
             promise.fulfill()
         }
         wait(for: [promise], timeout: 5)
-        print("exists", exists)
         //then
         XCTAssertTrue(exists)
     }
+    
+    func testSetDataForCurrentUser() throws {
+        try XCTSkipUnless(
+            networkMonitor.isReachable,
+            "Network connectivity needed for this test.")
+        //given
+        let key = "bio"
+        let value = "test"
+        let promise = expectation(description: "Completion handler invoked without error")
+        var didSave = false
+        //when
+        let docRef = sut.collection(K.FStore.usersCollection).document(K.FStore.currentUserId)
+        docRef.setData([
+            key: value,
+        ], merge: true) { error in
+            guard error == nil else {
+                XCTFail("Error: \(error?.localizedDescription ?? "")")
+                return
+            }
+            didSave = true
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+        //then
+        XCTAssertTrue(didSave)
+    }
+    
+    func testReadUserData() throws {
+        try XCTSkipUnless(
+            networkMonitor.isReachable,
+            "Network connectivity needed for this test.")
+        //given
+        let promise = expectation(description: "Completion handler invoked without error")
+        var dataReceived = false
+        let uid = K.FStore.currentUserId
+        //when
+        let docRef = sut.collection(K.FStore.usersCollection).document(uid)
+        docRef.getDocument { (document, error) in
+            guard error == nil else {
+                XCTFail("Error: \(error?.localizedDescription ?? "")")
+                return
+            }
+            if let document = document, document.exists {
+                dataReceived = true
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+        //then
+        XCTAssertTrue(dataReceived)
+    }
+    
+    func testQueryForUsersInLocation(){
+        //given
+        let promise = expectation(description: "Completion handler invoked without error")
+        let currentLocation = "Apple Campus"
+        var didQueryForUsers = false
+        //when
+        let docRef = sut.collection(K.FStore.usersCollection).whereField("postalCity", isEqualTo: currentLocation).limit(to: 20)
+        docRef.getDocuments() { querySnapshot, error in
+            guard error == nil else {
+                XCTFail("Error: \(error?.localizedDescription ?? "")")
+                return
+            }
+            if let snapshotDocuments = querySnapshot?.documents {
+                for doc in snapshotDocuments {
+                    let nearbyUserId = doc.documentID
+                    if nearbyUserId != K.FStore.currentUserId {
+                        didQueryForUsers = true;
+                    }
+                }
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+        //then
+        XCTAssertTrue(didQueryForUsers)
+    }
+    
+    
 
 }
